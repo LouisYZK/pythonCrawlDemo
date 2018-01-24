10d641b97fff2909a2abd6f065dc0180fc47d892
louis <yangzhikai@cug.edu.cn>
2018-01-24 11:46:53 +0800

    *

--- 1.23/crawl_1.23.py
+++ 1.23/crawl_1.23.py
@@ -25,47 +25,47 @@ msg = messaging.RemotingMessage(messageId=str(uuid.uuid1()).upper(),
                                     timeToLive=0,  
                                     timestamp=0)  
 # 第一个是查询参数，第二个是页数，第三个是控制每页显示的数量（默认每页只显示15条）
-def getPageNumber():
-    msg.body = [HqPara(),'1', '15']  
+
+# 构造请求数据
+def getRequestData(page_num,total_num):
+    msg.body = [HqPara(),str(page_num), str(total_num)]  
     msg.headers['DSEndpoint'] = None  
     msg.headers['DSId'] = str(uuid.uuid1()).upper()  
     # 按AMF协议编码数据  
     req = remoting.Request('null', body=(msg,))  
     env = remoting.Envelope(amfVersion=pyamf.AMF3)  
     env.bodies = [('/1', req)]  
-    data = bytes(remoting.encode(env).read())        
-    # 提交请求  
-    url = 'http://jgsb.agri.cn/messagebroker/amf'  
-    req = request.Request(url, data, headers={'Content-Type': 'application/x-amf'})  
-    # 解析返回数据  
-    opener = request.build_opener()     
-    # 解码AMF协议返回的数据  
-    resp = remoting.decode(opener.open(req).read())  
-    return resp.bodies[0][1].body.body[3]
-# body[3]是数据总条数，body[2]是页数
-def repsonse(i):
-# 发送请求函数，参数i为数据条数
-    msg.body = [HqPara(),'1',str(i)]  
-    msg.headers['DSEndpoint'] = None  
-    msg.headers['DSId'] = str(uuid.uuid1()).upper()  
-    # 按AMF协议编码数据  
-    req = remoting.Request('null', body=(msg,))  
-    env = remoting.Envelope(amfVersion=pyamf.AMF3)  
-    env.bodies = [('/1', req)]  
-    data = bytes(remoting.encode(env).read())       
-    # 提交请求  
-    url = 'http://jgsb.agri.cn/messagebroker/amf'  
+    data = bytes(remoting.encode(env).read())
+    return data
+# 获取响应源数据
+def getResponse(data):
+    url = 'http://jgsb.agri.cn/messagebroker/amf'
     req = request.Request(url, data, headers={'Content-Type': 'application/x-amf'})
     # 解析返回数据  
     opener = request.build_opener()          
-    # 解码AMF协议返回的数据  
-    resposne = remoting.decode(opener.open(req).read())
-    return resposne
+    return opener.open(req).read()
+# 解析
 def getContent(response):
-    fp = open('info.json','w')      
-    for i, record in enumerate(response.bodies[0][1].body.body[0]):  
+    amf_parse_info = remoting.decode(response)
+    # 数据总条数
+    total_num = amf_parse_info.bodies[0][1].body.body[3]
+    info = amf_parse_info.bodies[0][1].body.body[0]
+    return total_num, info
+def store2json(info):
+    res = []
+    for record in info:
         record['reportDate'] = record['reportDate'].strftime('%Y-%m-%d %H:%M:%S')
         record['auditDate'] = record['auditDate'].strftime('%Y-%m-%d %H:%M:%S')
-        json.dump(record,fp)    
-n = int(getPageNumber())
-getContent(repsonse(n))
+        res.append(record)
+    fp = open('info.json','w') 
+    json.dump(res,fp)
+    fp.close()
+# 测试
+reqData = getRequestData(1,15)
+rep = getResponse(reqData)
+total_num,info = getContent(rep)
+
+reqData = getRequestData(1,total_num)
+rep = getResponse(reqData)
+total_num,info = getContent(rep)
+store2json(info)
